5553286e0cc40fd85545b90605d82392dcb97ac9
Jeremy Gautrais <j.gautrais@thecodingmachine.com>
2024-02-08 11:28:45 +0100

    try new build

--- platform/next.config.js
+++ platform/next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  distDir: 'dist'
+  distDir: 'dist',
+  output: 'standalone'
 }
 
 module.exports = nextConfig
