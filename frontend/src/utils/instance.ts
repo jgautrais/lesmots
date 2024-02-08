c2a20afe3961d45c710241f4fd0b0bf1d6267eb0
Jeremy Gautrais <j.gautrais@thecodingmachine.com>
2024-02-08 17:14:49 +0100

    update arch

---
+++ frontend/src/utils/instance.ts
@@ -0,0 +1,21 @@
+import ky from 'ky';
+
+export const instance = ky.extend({
+    hooks: {
+        beforeRequest: [
+            request => {
+                const token = document.cookie
+                    .split('; ')
+                    .find(row => row.startsWith('XSRF-TOKEN='))
+                    ?.split('=')[1];
+
+                request.headers.set('X-XSRF-TOKEN', token ?? '');
+            }
+        ]
+    },
+    prefixUrl: import.meta.env.VITE_BACKEND_URL,
+    headers: {
+        Accept: 'application/json',
+    },
+    credentials: "include"
+});
