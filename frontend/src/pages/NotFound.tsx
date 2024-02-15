06d1b5e22075c4d1f4c8513a785ee4a6f78b070f
Jeremy Gautrais <j.gautrais@thecodingmachine.com>
2024-02-15 19:00:14 +0100

    small opti

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -1,6 +1,9 @@
+import { NavBar } from '@/components/molecules';
+
 function NotFound() {
   return (
-    <div>
+    <div className="container mx-auto text-center">
+      <NavBar />
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
     </div>
