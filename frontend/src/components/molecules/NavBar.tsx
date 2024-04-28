fed6fb0aac68a76c62ccc1cce949e9b3e7242afe
Jeremy Gautrais <j.gautrais@thecodingmachine.com>
2024-04-28 15:35:45 +0200

    adding theme switcher

--- frontend/src/components/molecules/NavBar.tsx
+++ frontend/src/components/molecules/NavBar.tsx
@@ -1,6 +1,23 @@
+import { useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
+import { useTheme, useSwitchTheme } from '@/hooks/theme.store';
+import { DarkThemeIcon, LightThemeIcon } from '@/components/atoms';
 
 function NavBar() {
+  const theme = useTheme();
+  const switchTheme = useSwitchTheme();
+
+  useEffect(() => {
+    const localTheme = JSON.parse(localStorage.getItem('theme') ?? '');
+    if (localTheme) {
+      document.documentElement.setAttribute(
+        'data-mode',
+        localTheme.state.theme
+      );
+      document.documentElement.className = localTheme.state.theme;
+    }
+  }, [theme]);
+
   return (
     <header className="container mx-auto w-full">
       <NavLink to="/">
@@ -8,6 +25,16 @@ function NavBar() {
           Les Mots
         </h1>
       </NavLink>
+      <button
+        className="absolute top-9 right-4 md:right-8"
+        onClick={() => switchTheme()}
+      >
+        {theme === 'dark' ? (
+          <LightThemeIcon className="text-gray-100" />
+        ) : (
+          <DarkThemeIcon className="text-slate-700" />
+        )}
+      </button>
     </header>
   );
 }
